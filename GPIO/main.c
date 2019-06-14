554aab6b954deea58af5254cf6c5f483a3b2464f
Jurqaa <jura9814@gmail.com>
2019-06-14 14:04:28 +0200

    Test

--- GPIO/main.c
+++ GPIO/main.c
@@ -95,7 +95,7 @@ enum KeyboardState eKeyboardRead(void){
 void KeyboardInit(void){
   IO0DIR = IO0DIR & (~(BUTT1_bm | BUTT2_bm | BUTT3_bm | BUTT4_bm));
 }
-//git test
+//git
 int main(void){
 
   LedInit();
